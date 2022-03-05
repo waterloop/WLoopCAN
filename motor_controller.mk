961f8edc9d0140c04be9bbd6b3f0e6dd27ea6dad
Ryan Mah <rmah@uwaterloo.ca>
2022-03-05 14:37:10 -0500

    fixed FPU mistake for motorcontroller

--- motor_controller.mk
+++ motor_controller.mk
@@ -9,6 +9,6 @@ DEVICE = STM32F405RGTx
 DEVICE_VARIANT = STM32F405xx
 DEVICE_FAMILY = STM32F4xx
 
-CPU = -mcpu=cortex-m0
-#FPU = -mfpu=fpv4-sp-d16
-FLOAT-ABI = -mfloat-abi=soft
+CPU = -mcpu=cortex-m4
+FPU = -mfpu=fpv4-sp-d16
+FLOAT-ABI = -mfloat-abi=hard
